8abc627b25eb79b61ee39ebe1354d5e5ef95c526
vladimir <vladimir.chernokulsky@gmail.com>
2021-06-20 18:17:56 +0300

    Buildroot package discription files

---
+++ orangepi-3/package/auto_sdl_game.mk
@@ -0,0 +1,9 @@
+SDL_GAME_VERSION = 0.01
+SDL_GAME_SOURCE = sdl_game-$(SDL_GAME_VERSION).tar.gz
+SDL_GAME_SITE = http://intemslab.ru
+SDL_GAME_INSTALL_STAGING = YES
+SDL_GAME_INSTALL_TARGET = YES
+SDL_GAME_DEPENDENCIES = sdl2 host-pkgconf
+
+#$(eval $(autotools-package))
+$(eval $(cmake-package))
