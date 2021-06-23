f38374eff2838ece68b7339999e64df51e34cc34
vladimir <vladimir.chernokulsky@gmail.com>
2021-06-23 12:26:28 +0300

    Add all SDL2 dependencies for sdl-game package for fix failed build process

--- orangepi-3/package/sdl_game/auto_sdl_game.mk
+++ orangepi-3/package/sdl_game/auto_sdl_game.mk
@@ -3,7 +3,7 @@ SDL_GAME_SOURCE = sdl_game-$(SDL_GAME_VERSION).tar.gz
 SDL_GAME_SITE = http://intemslab.ru
 SDL_GAME_INSTALL_STAGING = YES
 SDL_GAME_INSTALL_TARGET = YES
-SDL_GAME_DEPENDENCIES = sdl2 host-pkgconf
+SDL_GAME_DEPENDENCIES = sdl2 sdl2_mixer sdl2_image sdl2_ttf host-pkgconf
 
 #$(eval $(autotools-package))
 $(eval $(cmake-package))
